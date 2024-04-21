f2dae050c852c5617ed9e5284430b196fe5e7bf2
Grasaia <sdugger@hawk.iit.edu>
2024-04-20 23:54:57 -0500

    aligning buttons, removing empty spaces, fixing js code

--- js/scripts.js
+++ js/scripts.js
@@ -9,27 +9,27 @@ function signup() {
 }
 
 function changeCity(cityCode) {
-  var cityNameElement = document.getElementById('cityname');
-  switch(cityCode) {
-      case 'chc':
-          cityNameElement.textContent = 'Chicago';
-          break;
-      case 'nch':
-          cityNameElement.textContent = 'North Chicagoland';
-          break;
-      case 'wcl':
-          cityNameElement.textContent = 'West Chicagoland';
-          break;
-      case 'sox':
-          cityNameElement.textContent = 'South Chicagoland';
-          break;
-      case 'nwi':
-          cityNameElement.textContent = 'Northwest Indiana';
-          break;
-      case 'nwc':
-          cityNameElement.textContent = 'Northwest Suburbs';
-          break;
-      default:
-          break;
-  }
+    var cityNameElement = document.getElementById('cityname');
+    switch(cityCode) {
+        case 'chc':
+            cityNameElement.textContent = 'Chicago';
+            break;
+        case 'nch':
+            cityNameElement.textContent = 'North Chicagoland';
+            break;
+        case 'wcl':
+            cityNameElement.textContent = 'West Chicagoland';
+            break;
+        case 'sox':
+            cityNameElement.textContent = 'South Chicagoland';
+            break;
+        case 'nwi':
+            cityNameElement.textContent = 'Northwest Indiana';
+            break;
+        case 'nwc':
+            cityNameElement.textContent = 'Northwest Suburbs';
+            break;
+        default:
+            break;
+    }
 }
